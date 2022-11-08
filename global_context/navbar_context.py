0b8060af84c8cbfeae5e958ec079911416a40bfb
RobinGJonsson <63163226+RobinGJonsson@users.noreply.github.com>
2022-11-08 11:04:49 +0100

    Changed the page titles to be more SEO

--- global_context/navbar_context.py
+++ global_context/navbar_context.py
@@ -9,12 +9,15 @@ def navbar_context(request):
     memberships = Membership.objects.all()
     categories = Category.objects.all()
 
+    chepest_price = memberships.get(name="Bronze").monthly_price
+
     GOOGLE_MAPS_SECRET_KEY = settings.GOOGLE_MAPS_SECRET_KEY
 
     context = {
         'gyms': gyms,
         'memberships': memberships,
         'categories': categories,
+        'chepest_price': chepest_price,
     }
 
     return context
